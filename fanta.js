9249bbd7c99e6d5440e70443d626e310a88a1bba
Storm <psostorm@gmail.com>
2023-07-03 12:27:59 +0200

    the funnzy

--- fanta.js
+++ fanta.js
@@ -1,3 +1,22 @@
-import { compare } from "./parseFanta.js";
+import fs from 'node:fs';
+import path from 'node:path';
 
-console.log(compare("Mario kart"));
+import { FuzzySearcher } from './parseFanta.js';
+
+const input = process.argv[2] || 'Zelda twilight princess';
+
+function sanitize(word) {
+  return word
+    .trim()
+    .toLowerCase()
+    .replace('the ', '')
+    .replace(/[*"!.:?/-]/g, '');
+}
+
+const fuz = new FuzzySearcher(...fs.readFileSync(path.resolve(process.cwd(), 'sample.txt')).toString().split('\n').map(sanitize));
+
+const result = fuz.compare(sanitize(input), 0);
+
+console.log(input);
+console.table(fuz.vocabulary);
+console.table(result);
--- parseFanta.js
+++ parseFanta.js
@@ -1,25 +1,9 @@
-import fs from "node:fs";
-import path from "node:path";
+function nGram(n, doc) {
+  const str = `__${doc}`;
 
-function sanitize(word) {
-  return word
-    .trim()
-    .toLowerCase()
-    .replace("the ", "")
-    .replace(/\[.*?\]/g, "")
-    .replace(/\W/g, "");
-}
-
-const docs = fs
-  .readFileSync(path.resolve(process.cwd(), "sample.txt"))
-  .toString()
-  .split("\n")
-  .map(sanitize);
-
-function nGram(n, str) {
   const nGrams = [];
   for (let i = 0; i < str.length; i++) {
-    const slice = str.slice(i, i + n).padEnd(n, "_");
+    const slice = str.slice(i, i + n).padEnd(n, '_');
     nGrams.push(slice);
   }
   return nGrams;
@@ -27,36 +11,11 @@ function nGram(n, str) {
 
 const triGram = (str) => nGram(3, str);
 
-const gramsByDocument = docs.map((doc) => triGram(doc));
-const vocabulary = Array.from(new Set(gramsByDocument.flat()));
-const vectors = new Map();
-let max = 0;
-const min = 0; // can be incorrect but it's always going to be the case in my case
+const wordGram = (doc) => {
+  return doc.split(' ').filter((w) => w && w !== ' ');
+};
 
 const normalize = (n, min, max) => (n - min) / (max - min);
-
-for (let i = 0; i < gramsByDocument.length; i++) {
-  const grams = gramsByDocument[i];
-  const key = docs[i];
-  const vec = new Array(vocabulary.length).fill(0);
-
-  grams.forEach((gram) => {
-    const gramIndex = vocabulary.indexOf(gram);
-    const rawValue = ++vec[gramIndex];
-
-    max = Math.max(max, rawValue);
-  });
-
-  vectors.set(key, vec);
-}
-
-// normalize
-for (const [key, vector] of vectors) {
-  vectors.set(
-    key,
-    vector.map((n) => normalize(n, min, max))
-  );
-}
 
 function dotProduct(x, y) {
   function dotpSum(a, b) {
@@ -69,29 +28,83 @@ function dotProduct(x, y) {
 }
 
 function cosineSimilarity(A, B) {
-  return (
-    dotProduct(A, B) /
-    (Math.sqrt(dotProduct(A, A)) * Math.sqrt(dotProduct(B, B)))
-  );
+  return dotProduct(A, B) / (Math.sqrt(dotProduct(A, A)) * Math.sqrt(dotProduct(B, B)));
 }
 
-const compare = (str, threshold = 0.3) => {
-  const grams = triGram(sanitize(str));
-  const vec = new Array(vocabulary.length).fill(0);
+function FuzzySearcher(...intialDocuments) {
+  this.documents = [...intialDocuments];
+  this.gramsByDocument = null;
+  this.vocabulary = null;
+  this.vocabularyIndex = null;
+  this.vectors = new Map();
+  this.min = 0; // can be incorrect but it's always going to be the case in this case
+  this.max = 0;
 
-  grams.forEach((gram) => {
-    const gramIndex = vocabulary.indexOf(gram);
-    if (gramIndex > -1) ++vec[gramIndex];
-  });
+  this.strToGrams = triGram;
 
-  const similar = [];
+  this.bagOfWords = () => {
+    // build grams for each document
+    // build vocabulary from unique grams
+    // index vocabulary for easy access
+    // build vectors by counting how many times a specific gram appears in a document
+    // result: massive 3k long vectors, not great
 
-  for (const [key, tVec] of vectors) {
-    const similarity = cosineSimilarity(vec, tVec);
-    if (similarity > threshold) similar.push([key, similarity]);
-  }
+    this.gramsByDocument = this.documents.map((d) => this.strToGrams(d));
+    this.vocabulary = Array.from(new Set(this.gramsByDocument.flat()));
+    this.vocabularyIndex = new Map(this.vocabulary.map((v, i) => [v, i]));
 
-  return similar.sort(([, a], [, b]) => b - a);
-};
+    for (let i = 0; i < this.gramsByDocument.length; i++) {
+      const grams = this.gramsByDocument[i];
+      const key = this.documents[i];
+      const vector = new Array(this.vocabulary.length).fill(0);
 
-export { vectors, vocabulary, compare };
+      grams.forEach((gram) => {
+        const gramIndex = this.vocabularyIndex.get(gram);
+        const rawValue = ++vector[gramIndex];
+        this.max = Math.max(this.max, rawValue);
+      });
+
+      this.vectors.set(key, vector);
+    }
+  };
+
+  this.parseDocuments = () => {
+    this.bagOfWords();
+
+    // normalize
+    for (const [key, vector] of this.vectors) {
+      this.vectors.set(
+        key,
+        vector.map((n) => normalize(n, this.min, this.max)),
+      );
+    }
+  };
+
+  this.addDocs = (...documents) => {
+    this.documents.push(...documents);
+    this.parseDocuments();
+  };
+
+  this.compare = (search, threshold = 0) => {
+    const seachGrams = this.strToGrams(search);
+    const searchVector = new Array(this.vocabulary.length).fill(0);
+
+    seachGrams.forEach((gram) => {
+      const gramIndex = this.vocabularyIndex.get(gram);
+      if (gramIndex) ++searchVector[gramIndex];
+    });
+
+    const similar = [];
+
+    for (const [key, targetVector] of this.vectors) {
+      const similarity = cosineSimilarity(searchVector, targetVector);
+      if (similarity > threshold) similar.push([key, similarity]);
+    }
+
+    return similar.sort(([, a], [, b]) => b - a);
+  };
+
+  this.parseDocuments();
+}
+
+export { FuzzySearcher };
